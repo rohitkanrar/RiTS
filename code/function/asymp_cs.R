062ad3425f349b04f615d86d8218d3e1137ba9ba
rohitkanrar <rohitk@iastate.edu>
2024-09-03 13:44:02 -0500

    modified asympcs in online setup

--- code/function/asymp_cs.R
+++ code/function/asymp_cs.R
@@ -1,5 +1,8 @@
-get_aipw_score <- function(t, K, trt_hist, rwd_hist, prpns_mat){
-  ind1 <- sample(1:t, floor(t/2))
+get_aipw_score <- function(t, K, trt_hist, rwd_hist, prpns_mat, tr_ind){
+  if(t < 3 || length(tr_ind) == 0){
+    stop("Too early to obtain AsympCS. Gather more data...")
+  }
+  ind1 <- tr_ind
   ind2 <- setdiff(1:t, ind1)
   trt_hist1 <- trt_hist[ind1]
   trt_hist2 <- trt_hist[ind2]
@@ -28,7 +31,7 @@ get_aipw_score <- function(t, K, trt_hist, rwd_hist, prpns_mat){
   aipw_mat
 }
 
-get_aipw_var_running <- function(psi, psi_running){
+get_aipw_var_running <- function(psi, tr_ind){
   t <- length(psi)
   var_running <- numeric(t)
   for(i in 1:t){
@@ -37,37 +40,59 @@ get_aipw_var_running <- function(psi, psi_running){
   var_running
 }
 
-get_asympCS <- function(aipw_mat, trt_arm, placebo_arm, alpha,
-                        first_peek = 50){
-  t <- nrow(aipw_mat)
-  m <- first_peek
+get_rho <- function(first_peek, psi_var, alpha = 0.05){
+  m <- first_peek; e <- exp(1)
+  rho <- -2 * log(alpha) + log(-2 * log(alpha)) + 1
+  rho <- rho / (psi_var * m * log(max(c(m, e))))
+  rho <- sqrt(rho)
+  rho
+}
+
+get_ugly_term <- function(t, rho, alpha){
+  out <- 2 * (t * rho^2 + 1) * log(sqrt(t * rho^2 + 1) / alpha)
+  out <- out / (t * rho)^2
+  sqrt(out)
+}
+
+get_asympcs <- function(trt_hist, rwd_hist, prpns_mat,
+                        trt_arm, placebo_arm, alpha = 0.05, first_peek = 50){
+  # browser()
+  T_ <- length(trt_hist); K <- length(unique(trt_hist)); m <- first_peek
+  tr_ind <- sample(1:m, floor(m/2), replace = F)
+  te_ind <- setdiff(1:m, tr_ind)
   e <- exp(1)
-  asymp_cs <- matrix(0, t, 3)
-  psi <- aipw_mat[, trt_arm] - aipw_mat[, placebo_arm]
-  psi_running <- cumsum(psi) / (1:t)
-  var_running <- get_aipw_var_running(psi, psi_running)
-  rho_m <- (-2 * log(alpha) + log(-2 * log(alpha)) + 1) / 
-    (var_running[m] * m * log(max(m, e)))
-  rho_m <- sqrt(rho_m)
+  asymp_cs <- matrix(0, T_, 3)
   
-  for(i in 1:t){
-    if(i < first_peek) next
-    # browser()
-    tmp <- i * var_running[i] * rho_m^2 + 1
-    tmp <- 2 * tmp * log(sqrt(tmp)/alpha) / (i * rho_m)^2
-    tmp <- sqrt(tmp)
-    asymp_cs[i, ] <- c(psi_running[i], psi_running[i] - tmp, 
-                       psi_running[i] + tmp)
+  for(t in m:T_){
+    if(t > m){
+      p <- rbinom(1, 1, 0.5)
+      if(p == 0){
+        tr_ind <- c(tr_ind, t)
+      } else{
+        te_ind <- c(te_ind, t)
+      }
+    }
+    
+    aipw_mat <- get_aipw_score(t, K, trt_hist, rwd_hist, prpns_mat, tr_ind)
+    psi1 <- aipw_mat[tr_ind, trt_arm] - aipw_mat[tr_ind, placebo_arm]
+    psi2 <- aipw_mat[te_ind, trt_arm] - aipw_mat[te_ind, placebo_arm]
+    psi <- (sum(psi1) + sum(psi2))/t
+    psi_var <- (var(psi1) + var(psi2))/2
+    if(t == m){
+      rho <- get_rho(first_peek, psi_var, alpha)
+    }
+    half_width <- sqrt(psi_var) * get_ugly_term(t, rho, alpha)
+    asymp_cs[t, ] <- c(psi, psi - half_width, psi + half_width)
   }
   asymp_cs
 }
 
+
 # trt <- ts_out$trt; rwd <- ts_out$reward; prpns_mat <- ts_out$log_dat$prpns_mat
 trt <- rits_out$trt; rwd <- rits_out$reward_benf; prpns_mat <- rits_out$log_dat$prpns_mat
 par(mfrow = c(1, 3))
-tmp <- get_aipw_score(length(trt), length(unique(trt)), trt, rwd, prpns_mat)
 for(k in 2:4){
-  tmp1 <- get_asympCS(tmp, k, 1, 0.05, first_peek = 30)
+  tmp1 <- get_asympcs(trt, rwd, prpns_mat, k, 1, 0.05, first_peek = 30)
   
   plot(tmp1[, 1], type = "l", ylim = c(-3, 3), xlab = "Patient", ylab = "ATE")
   lines(tmp1[, 2])
