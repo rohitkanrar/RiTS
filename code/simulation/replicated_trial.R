0c6213227eb06d06d068daacc0155baad0efa97a
rohitkanrar <rohitk@iastate.edu>
2025-04-14 15:34:21 -0500

    trying low-snr simulation

--- code/simulation/replicated_trial.R
+++ code/simulation/replicated_trial.R
@@ -15,7 +15,7 @@ d <- sim_choice$d # cannot be changed
 E <- 2 # cannot be changed
 X <- sim_dat$X_true
 X_true <- sim_dat$X_true
-beta_true <- sim_dat$beta_true * (2/5) # changed due to AE's comment
+beta_true <- sim_dat$beta_true * (1/5) # changed due to AE's comment
 placebo_arm <- sim_dat$placebo_arm
 weight <- sim_dat$weight
 tr_start <- sim_choice$tr_start
@@ -60,23 +60,23 @@ for(min_prpn in min_prpns){
                                       design = design, min_prpn = min_prpn, 
                                       first_peek = first_peek, alpha = alpha)
     
-    ts_mis_sim[[iter]] <- do_ts_batch(X, X_true, beta_true, seed = seed, weight = weight,
-                                  placebo_arm = placebo_arm, tr_start = tr_start,
-                                  ate_start = ate_start, rwd_sig = reward_sig,
-                                  design = design, min_prpn = min_prpn, 
-                                  first_peek = first_peek, alpha = alpha)
-    rits_mis_sim[[iter]] <- do_rits_batch(X, X_true, beta_true, weight = weight, seed = seed,
-                                      tr_start = tr_start, ate_start = ate_start,
-                                      placebo_arm = placebo_arm, rwd_sig = reward_sig,
-                                      design = design, min_prpn = min_prpn, 
-                                      first_peek = first_peek, alpha = alpha)
+    # ts_mis_sim[[iter]] <- do_ts_batch(X, X_true, beta_true, seed = seed, weight = weight,
+    #                               placebo_arm = placebo_arm, tr_start = tr_start,
+    #                               ate_start = ate_start, rwd_sig = reward_sig,
+    #                               design = design, min_prpn = min_prpn, 
+    #                               first_peek = first_peek, alpha = alpha)
+    # rits_mis_sim[[iter]] <- do_rits_batch(X, X_true, beta_true, weight = weight, seed = seed,
+    #                                   tr_start = tr_start, ate_start = ate_start,
+    #                                   placebo_arm = placebo_arm, rwd_sig = reward_sig,
+    #                                   design = design, min_prpn = min_prpn, 
+    #                                   first_peek = first_peek, alpha = alpha)
     
   }
   saveRDS(ts_sim, paste(out_dir, "ts_sim_", i, ".RData", sep = ""))
   saveRDS(rand_sim, paste(out_dir, "rand_sim_", i, ".RData", sep = ""))
   saveRDS(rits_sim, paste(out_dir, "rits_sim_", i, ".RData", sep = ""))
-  saveRDS(ts_mis_sim, paste(out_dir, "ts_mis_sim_", i, ".RData", sep = ""))
-  saveRDS(rits_mis_sim, paste(out_dir, "rits_mis_sim_", i, ".RData", sep = ""))
+  # saveRDS(ts_mis_sim, paste(out_dir, "ts_mis_sim_", i, ".RData", sep = ""))
+  # saveRDS(rits_mis_sim, paste(out_dir, "rits_mis_sim_", i, ".RData", sep = ""))
   i <- i + 1 
 }
 
