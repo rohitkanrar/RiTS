cbe471e4cf128da702b35aa199db1ffd28047c30
rohitkanrar <rohitk@iastate.edu>
2025-05-15 10:39:53 -0500

    tmp fixing rand outputs

---
+++ code/simulation/tmp_fix.R
@@ -0,0 +1,11 @@
+source("code/function/classical_ci.R")
+require(parallel)
+rand_files <- list.files("output/varyingX/", pattern = "^rand_")
+mclapply(1:length(rand_files), function(i){
+  flname <- paste("output/varyingX/", rand_files[i], sep = "")
+  batch <- 1
+  out_sim <- readRDS(flname)
+  out_sim <- add_standard_ci(out = out_sim, ate_start = 20, batch = batch, 
+                             placebo_arm = 1, alpha = 0.05, force_compute = TRUE)
+  saveRDS(object = out_sim, file = flname)
+}, mc.cores = 9)
