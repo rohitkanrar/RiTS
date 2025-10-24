b9fb71cdcaa63b216b901f3e463d41a950df9f08
rohitkanrar <rohitk@iastate.edu>
2025-10-24 18:28:43 -0500

    added o'brien fleming based repeated ci

--- code/simulation/replicated_trial.R
+++ code/simulation/replicated_trial.R
@@ -4,8 +4,8 @@ source("code/function/misc.R")
 
 sim_choice <- readRDS("metadata/sim_choice.RData")
 sim_dat <- readRDS("metadata/sim_dat.RData")
-dgps <- c("low", "high", "null")
-# dgps <- c("null")
+# dgps <- c("low", "high", "null")
+dgps <- c("null")
 tr_starts <- sim_choice$tr_start
 min_prpns <- sim_choice$min_prpns
 cases <- expand.grid(dgp = dgps, min_prpn = min_prpns, tr_start = tr_starts)
@@ -58,28 +58,26 @@ results <- mclapply(1:nrow(cases), function(i, cases, sim_choice, sim_dat, n_ite
     set.seed(seed_)
     X_true <- get_X(N = N)
     X <- X_true[, 1:2]
-    if(dgp == "null"){
-      ts_sim[[iter]] <- do_ts_batch(X = X_true, X_true = X_true, beta_true = beta_true, 
-                                    weight = weight, seed = seed_, rwd_sig = reward_sig,
-                                    tr_start = tr_start, tr_batch = 5, tr_lag = 10,
-                                    M = 1000, v = 10, min_prpn = min_prpn, asympcs = FALSE)
-      rits_sim[[iter]] <- do_rits_batch(X = X_true, X_true = X_true, beta_true = beta_true, 
-                                        weight = weight, seed = seed_, rwd_sig = reward_sig,
-                                        tr_start = tr_start, tr_batch = 5, tr_lag = 10,
-                                        M = 1000, v = 10, min_prpn = min_prpn, asympcs = FALSE)
-      ts_mis_sim[[iter]] <- do_ts_batch(X = X, X_true = X_true, beta_true = beta_true, 
-                                        weight = weight, seed = seed_, rwd_sig = reward_sig,
-                                        tr_start = tr_start, tr_batch = 5, tr_lag = 10,
-                                        M = 1000, v = 10, min_prpn = min_prpn, asympcs = FALSE)
-      rits_mis_sim[[iter]] <- do_rits_batch(X = X, X_true = X_true, beta_true = beta_true, 
-                                            weight = weight, seed = seed_, rwd_sig = reward_sig,
-                                            tr_start = tr_start, tr_batch = 5, tr_lag = 10,
-                                            M = 1000, v = 10, min_prpn = min_prpn, asympcs = FALSE)
-      saveRDS(ts_sim, paste(out_dir, "ts_sim_", case_str, ".RData", sep = ""))
-      saveRDS(rits_sim, paste(out_dir, "rits_sim_", case_str, ".RData", sep = ""))
-      saveRDS(ts_mis_sim, paste(out_dir, "ts_mis_sim_", case_str, ".RData", sep = ""))
-      saveRDS(rits_mis_sim, paste(out_dir, "rits_mis_sim_", case_str, ".RData", sep = ""))
-    }
+    ts_sim[[iter]] <- do_ts_batch(X = X_true, X_true = X_true, beta_true = beta_true, 
+                                  weight = weight, seed = seed_, rwd_sig = reward_sig,
+                                  tr_start = tr_start, tr_batch = 5, tr_lag = 10,
+                                  M = 1000, v = 10, min_prpn = min_prpn, asympcs = FALSE)
+    rits_sim[[iter]] <- do_rits_batch(X = X_true, X_true = X_true, beta_true = beta_true, 
+                                      weight = weight, seed = seed_, rwd_sig = reward_sig,
+                                      tr_start = tr_start, tr_batch = 5, tr_lag = 10,
+                                      M = 1000, v = 10, min_prpn = min_prpn, asympcs = FALSE)
+    ts_mis_sim[[iter]] <- do_ts_batch(X = X, X_true = X_true, beta_true = beta_true, 
+                                      weight = weight, seed = seed_, rwd_sig = reward_sig,
+                                      tr_start = tr_start, tr_batch = 5, tr_lag = 10,
+                                      M = 1000, v = 10, min_prpn = min_prpn, asympcs = FALSE)
+    rits_mis_sim[[iter]] <- do_rits_batch(X = X, X_true = X_true, beta_true = beta_true, 
+                                          weight = weight, seed = seed_, rwd_sig = reward_sig,
+                                          tr_start = tr_start, tr_batch = 5, tr_lag = 10,
+                                          M = 1000, v = 10, min_prpn = min_prpn, asympcs = FALSE)
+    saveRDS(ts_sim, paste(out_dir, "ts_sim_", case_str, ".RData", sep = ""))
+    saveRDS(rits_sim, paste(out_dir, "rits_sim_", case_str, ".RData", sep = ""))
+    saveRDS(ts_mis_sim, paste(out_dir, "ts_mis_sim_", case_str, ".RData", sep = ""))
+    saveRDS(rits_mis_sim, paste(out_dir, "rits_mis_sim_", case_str, ".RData", sep = ""))
     if(min_prpn == 0.005){
       rand_sim[[iter]] <- do_rand_biv(X = X_true, X_true = X_true, beta_true = beta_true, 
                                       weight = weight, seed = seed_, 
@@ -90,7 +88,7 @@ results <- mclapply(1:nrow(cases), function(i, cases, sim_choice, sim_dat, n_ite
                                           rwd_sig = reward_sig, tr_start = tr_start,
                                           asympcs = FALSE)
     }
-    }
+  }
   if(min_prpn == 0.005){
     saveRDS(rand_sim, paste(out_dir, "rand_sim_", case_str0, ".RData", sep = ""))
     saveRDS(rand_mis_sim, paste(out_dir, "rand_mis_sim_", case_str0, ".RData", sep = ""))
