5b2470e10199f65d973c6e97c2a36229514ed25d
rohitkanrar <rohitk@iastate.edu>
2025-11-09 20:56:32 -0600

    minor

--- code/visualization/appendix_tab.R
+++ code/visualization/appendix_tab.R
@@ -12,107 +12,107 @@ ind <- round(
 delay_aipw <- 57; delay_ipw = 0
 j <- 0
 
-# # cumulative mis-coverage and estimation error tables
-# cum_miscov_all_tab <- matrix(NA, nrow(cases), 7*(K-1))
-# cum_miscov_all_mis_tab <- matrix(NA, nrow(cases), 7*(K-1))
-# est_err_all_tab <- vector(mode = "list", length = nrow(cases))
-# est_err_all_mis_tab <- vector(mode = "list", length = nrow(cases))
-# 
-# for(i in 1:nrow(cases)){
-#   print(i)
-#   dgp <- cases[i, "dgp"]; min_prpn <- cases[i, "min_prpn"]
-#   tr_start <- cases[i, "tr_start"]
-#   case_str <- paste("dgp", dgp, "min_prpn", min_prpn, "tr_start", tr_start,
-#                     sep = "_")
-#   case_str0 <- paste("dgp", dgp, "min_prpn", 0.005, "tr_start", sim_choice$ate_start,
-#                      sep = "_")
-#   rand_file_name <- paste(out_dir, "rand_sim_", case_str0, ".RData", sep = "")
-#   rand_mis_file_name <- paste(out_dir, "rand_mis_sim_", case_str0, ".RData", sep = "")
-#   ts_file_name <- paste(out_dir, "ts_sim_", case_str, ".RData", sep = "")
-#   ts_mis_file_name <- paste(out_dir, "ts_mis_sim_", case_str, ".RData", sep = "")
-#   rits_file_name <- paste(out_dir, "rits_sim_", case_str, ".RData", sep = "")
-#   rits_mis_file_name <- paste(out_dir, "rits_mis_sim_", case_str, ".RData", sep = "")
-# 
-#   if(dgp == "high"){
-#     mu_true <- sim_dat$mu_true * 2
-#   } else{
-#     mu_true <- sim_dat$mu_true
-#   }
-#   contr_true <- mu_true - mu_true[1]
-#   contr_true <- contr_true[setdiff(1:K, sim_dat$placebo_arm)]
-#   ## Correctly specified
-#   # cumulative mis-coverage
-#   rand_sim <- readRDS(rand_file_name); rand_sim <- rand_sim[1:n_iter]
-#   if(j == 0){
-#     ate_ind <- sapply(ind, function(i){
-#       which(as.numeric(dimnames(rand_sim[[1]]$contr)[[1]]) == i)
-#     })
-#     j <- j + 1
-#   }
-#   ts_sim <- readRDS(ts_file_name); ts_sim <- ts_sim[1:n_iter]
-#   rits_sim <- readRDS(rits_file_name); rits_sim <- rits_sim[1:n_iter]
-#   cum_miscov_all_tab[i, ] <- gen_cum_miscov_for_tab(out_rand = rand_sim,
-#                                                     out_ts = ts_sim,
-#                                                     out_rits = rits_sim,
-#                                                     mu_true = mu_true,
-#                                                     contr_true = contr_true,
-#                                                     delay_aipw = delay_aipw,
-#                                                     delay_ipw = delay_ipw)
-#   # estimation error (bias and rmse)
-#   summ_rand <- gen_summary_for_table(sim = rand_sim, K = K,
-#                                      ate_ind = ate_ind, contr_true = contr_true,
-#                                      need_std = TRUE, need_ipw = FALSE)
-#   summ_ts <- gen_summary_for_table(sim = ts_sim, K = K,
-#                                    ate_ind = ate_ind, contr_true = contr_true,
-#                                    need_ipw = FALSE)
-#   summ_rits <- gen_summary_for_table(sim = rits_sim, K = K,
-#                                      ate_ind = ate_ind, contr_true = contr_true,
-#                                      need_ipw = FALSE)
-# 
-#   est_err_tab <- gen_bias_rmse_tab(summ_rand = summ_rand,
-#                                        summ_ts = summ_ts,
-#                                        summ_rits = summ_rits,
-#                                        ate_ind = ate_ind, ind = ind)
-#   est_err_all_tab[[i]] <- list(tab = est_err_tab, case_str = case_str)
-#   ## Mis-specified
-#   # cumulative mis-coverage
-#   rand_sim <- readRDS(rand_mis_file_name); ts_sim <- readRDS(ts_mis_file_name);
-#   rits_sim <- readRDS(rits_mis_file_name)
-#   cum_miscov_all_mis_tab[i, ] <- gen_cum_miscov_for_tab(out_rand = rand_sim,
-#                                                     out_ts = ts_sim,
-#                                                     out_rits = rits_sim,
-#                                                     mu_true = mu_true,
-#                                                     contr_true = contr_true,
-#                                                     delay_aipw = delay_aipw,
-#                                                     delay_ipw = delay_ipw)
-#   # estimation error (bias and rmse)
-#   summ_rand <- gen_summary_for_table(sim = rand_sim, K = K,
-#                                      ate_ind = ate_ind, contr_true = contr_true,
-#                                      need_std = TRUE, need_ipw = FALSE)
-#   summ_ts <- gen_summary_for_table(sim = ts_sim, K = K,
-#                                    ate_ind = ate_ind, contr_true = contr_true,
-#                                    need_ipw = FALSE)
-#   summ_rits <- gen_summary_for_table(sim = rits_sim, K = K,
-#                                      ate_ind = ate_ind, contr_true = contr_true,
-#                                      need_ipw = FALSE)
-# 
-#   est_err_tab <- gen_bias_rmse_tab(summ_rand = summ_rand,
-#                                    summ_ts = summ_ts,
-#                                    summ_rits = summ_rits,
-#                                    ate_ind = ate_ind, ind = ind)
-#   est_err_all_mis_tab[[i]] <- list(tab = est_err_tab, case_str = case_str)
-# }
-# 
-# saveRDS(cum_miscov_all_tab, "tables/cum_miscov_all_tab.RData")
-# saveRDS(cum_miscov_all_mis_tab, "tables/cum_miscov_all_mis_tab.RData")
-# saveRDS(est_err_all_tab, "tables/est_err_all_tab.RData")
-# saveRDS(est_err_all_mis_tab, "tables/est_err_all_mis_tab.RData")
-# 
-# # cum_miscov_all_tab <- readRDS("tables/cum_miscov_all_tab.RData")
-# # cum_miscov_all_mis_tab <- readRDS("tables/cum_miscov_all_mis_tab.RData")
-# # est_err_all_tab <- readRDS("tables/est_err_all_tab.RData")
-# # est_err_all_mis_tab <- readRDS("tables/est_err_all_mis_tab.RData")
-# # xtable::xtable(rbind(est_err_all_mis_tab[[22]]$tab, est_err_all_mis_tab[[10]]$tab))
+# cumulative mis-coverage and estimation error tables
+cum_miscov_all_tab <- matrix(NA, nrow(cases), 7*(K-1))
+cum_miscov_all_mis_tab <- matrix(NA, nrow(cases), 7*(K-1))
+est_err_all_tab <- vector(mode = "list", length = nrow(cases))
+est_err_all_mis_tab <- vector(mode = "list", length = nrow(cases))
+
+for(i in 1:nrow(cases)){
+  print(i)
+  dgp <- cases[i, "dgp"]; min_prpn <- cases[i, "min_prpn"]
+  tr_start <- cases[i, "tr_start"]
+  case_str <- paste("dgp", dgp, "min_prpn", min_prpn, "tr_start", tr_start,
+                    sep = "_")
+  case_str0 <- paste("dgp", dgp, "min_prpn", 0.005, "tr_start", sim_choice$ate_start,
+                     sep = "_")
+  rand_file_name <- paste(out_dir, "rand_sim_", case_str0, ".RData", sep = "")
+  rand_mis_file_name <- paste(out_dir, "rand_mis_sim_", case_str0, ".RData", sep = "")
+  ts_file_name <- paste(out_dir, "ts_sim_", case_str, ".RData", sep = "")
+  ts_mis_file_name <- paste(out_dir, "ts_mis_sim_", case_str, ".RData", sep = "")
+  rits_file_name <- paste(out_dir, "rits_sim_", case_str, ".RData", sep = "")
+  rits_mis_file_name <- paste(out_dir, "rits_mis_sim_", case_str, ".RData", sep = "")
+
+  if(dgp == "high"){
+    mu_true <- sim_dat$mu_true * 2
+  } else{
+    mu_true <- sim_dat$mu_true
+  }
+  contr_true <- mu_true - mu_true[1]
+  contr_true <- contr_true[setdiff(1:K, sim_dat$placebo_arm)]
+  ## Correctly specified
+  # cumulative mis-coverage
+  rand_sim <- readRDS(rand_file_name); rand_sim <- rand_sim[1:n_iter]
+  if(j == 0){
+    ate_ind <- sapply(ind, function(i){
+      which(as.numeric(dimnames(rand_sim[[1]]$contr)[[1]]) == i)
+    })
+    j <- j + 1
+  }
+  ts_sim <- readRDS(ts_file_name); ts_sim <- ts_sim[1:n_iter]
+  rits_sim <- readRDS(rits_file_name); rits_sim <- rits_sim[1:n_iter]
+  cum_miscov_all_tab[i, ] <- gen_cum_miscov_for_tab(out_rand = rand_sim,
+                                                    out_ts = ts_sim,
+                                                    out_rits = rits_sim,
+                                                    mu_true = mu_true,
+                                                    contr_true = contr_true,
+                                                    delay_aipw = delay_aipw,
+                                                    delay_ipw = delay_ipw)
+  # estimation error (bias and rmse)
+  summ_rand <- gen_summary_for_table(sim = rand_sim, K = K,
+                                     ate_ind = ate_ind, contr_true = contr_true,
+                                     need_std = TRUE, need_ipw = FALSE)
+  summ_ts <- gen_summary_for_table(sim = ts_sim, K = K,
+                                   ate_ind = ate_ind, contr_true = contr_true,
+                                   need_ipw = FALSE)
+  summ_rits <- gen_summary_for_table(sim = rits_sim, K = K,
+                                     ate_ind = ate_ind, contr_true = contr_true,
+                                     need_ipw = FALSE)
+
+  est_err_tab <- gen_bias_rmse_tab(summ_rand = summ_rand,
+                                       summ_ts = summ_ts,
+                                       summ_rits = summ_rits,
+                                       ate_ind = ate_ind, ind = ind)
+  est_err_all_tab[[i]] <- list(tab = est_err_tab, case_str = case_str)
+  ## Mis-specified
+  # cumulative mis-coverage
+  rand_sim <- readRDS(rand_mis_file_name); ts_sim <- readRDS(ts_mis_file_name);
+  rits_sim <- readRDS(rits_mis_file_name)
+  cum_miscov_all_mis_tab[i, ] <- gen_cum_miscov_for_tab(out_rand = rand_sim,
+                                                    out_ts = ts_sim,
+                                                    out_rits = rits_sim,
+                                                    mu_true = mu_true,
+                                                    contr_true = contr_true,
+                                                    delay_aipw = delay_aipw,
+                                                    delay_ipw = delay_ipw)
+  # estimation error (bias and rmse)
+  summ_rand <- gen_summary_for_table(sim = rand_sim, K = K,
+                                     ate_ind = ate_ind, contr_true = contr_true,
+                                     need_std = TRUE, need_ipw = FALSE)
+  summ_ts <- gen_summary_for_table(sim = ts_sim, K = K,
+                                   ate_ind = ate_ind, contr_true = contr_true,
+                                   need_ipw = FALSE)
+  summ_rits <- gen_summary_for_table(sim = rits_sim, K = K,
+                                     ate_ind = ate_ind, contr_true = contr_true,
+                                     need_ipw = FALSE)
+
+  est_err_tab <- gen_bias_rmse_tab(summ_rand = summ_rand,
+                                   summ_ts = summ_ts,
+                                   summ_rits = summ_rits,
+                                   ate_ind = ate_ind, ind = ind)
+  est_err_all_mis_tab[[i]] <- list(tab = est_err_tab, case_str = case_str)
+}
+
+saveRDS(cum_miscov_all_tab, "tables/cum_miscov_all_tab.RData")
+saveRDS(cum_miscov_all_mis_tab, "tables/cum_miscov_all_mis_tab.RData")
+saveRDS(est_err_all_tab, "tables/est_err_all_tab.RData")
+saveRDS(est_err_all_mis_tab, "tables/est_err_all_mis_tab.RData")
+
+# cum_miscov_all_tab <- readRDS("tables/cum_miscov_all_tab.RData")
+# cum_miscov_all_mis_tab <- readRDS("tables/cum_miscov_all_mis_tab.RData")
+# est_err_all_tab <- readRDS("tables/est_err_all_tab.RData")
+# est_err_all_mis_tab <- readRDS("tables/est_err_all_mis_tab.RData")
+# xtable::xtable(rbind(est_err_all_mis_tab[[22]]$tab, est_err_all_mis_tab[[10]]$tab))
 
 
 ######################################
